5389be69fb9429e4bc04a13f6a6ee8859f19fcd8
Manikantan <157674543+Mani-kl@users.noreply.github.com>
2024-01-25 19:27:53 +0800

    Add files via upload

---
+++ Scripts_Insert.sql
@@ -0,0 +1,20 @@
+USE [TESTEFDB]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([UserName]
+           ,[Email]
+           ,[Mobile]
+           ,[Skills]
+           ,[Hobby]
+          )
+     VALUES
+           ('Mani123',
+           'mani@yopmail.com',
+           '012232232',
+           'C#.Net',
+           'Reading')
+      
+GO
+
+
